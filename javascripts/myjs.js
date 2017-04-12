b1d5276ba6738064bc8c1576913739f801a0b61f
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-04-12 00:15:46 -0400

    using javascript to set 'static' height for landing-background

--- javascripts/myjs.js
+++ javascripts/myjs.js
@@ -1,3 +1,9 @@
-//new WOW().init();
+var bg = $("#landing-background");
 
+function resizeBackground() {
+    bg.height($(window).height());
+}
+
+$(window).resize(resizeBackground);
+resizeBackground();
 AOS.init();
